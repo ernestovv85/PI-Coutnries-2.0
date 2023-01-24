8cc90924855aca47577974a04daaa14cba54df02
ernestovv85 <ernestovv85@gmail.com>
2023-01-24 15:04:21 -0600

    add components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePageComponent } from './components/create-page/create-page.component';
+import { DetailPageComponent } from './components/detail-page/detail-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LandingPageComponent},
+  {path:'home', component:HomePageComponent},
+  {path:'detail', component:DetailPageComponent},
+  {path:'create', component:CreatePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { CreatePageComponent } from './components/create-page/create-page.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { CardPageComponent } from './components/card-page/card-page.component';
+import { DetailPageComponent } from './components/detail-page/detail-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HomePageComponent,
+    CreatePageComponent,
+    SearchBarComponent,
+    CardPageComponent,
+    DetailPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
